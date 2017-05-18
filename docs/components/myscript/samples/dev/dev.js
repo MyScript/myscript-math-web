a51d94b03d834146cf58dd268878695147dc9368
Mathieu Ruellan <mathieu.ruellan@myscript.com>
2017-05-18 17:57:32 +0200

    [IINK] update iink preview url

--- docs/components/myscript/samples/dev/dev.js
+++ docs/components/myscript/samples/dev/dev.js
@@ -6,7 +6,7 @@ const editor = MyScript.register(myScriptEditorDomElement, {
     server: {
       scheme: 'http',
       host: 'localhost:8897',
-      applicationKey: 'ae64451b-9557-4eec-b991-59d41e6799f3',
+      applicationKey: '7d223f9e-a3cb-4213-ba4b-85e930605f8b',
       hmacKey: '5ab1935e-529a-4d48-a695-158450e52b13',
       websocket: {
         pingEnabled: false
